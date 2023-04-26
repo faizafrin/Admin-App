29170dd600a8bd5ed8a74c99ca82f0cfa4915dfe
faizafrin <faizasworld2022@gmail.com>
2023-04-26 20:37:07 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import React from "react";
+//import BaseApp from "./Base/base";
 import './App.css';
+import LogInPage from "./pages/LogInPage";
+import Register from "./pages/RegisterPage";
+import ForgotPassword from "./pages/forgot-password";
+import Dashboard from "./childrens/Dashboard";
+import Button from "./Components/button";
+import Cards from "./Components/card";
+import TablesReact from "./Table/table";
+import { Route, Routes } from "react-router";
+import ErrorPage from "./pages/ErrorPage";
+import BlankPage from "./pages/BlankPage";
+import Colors from "./utility/colors";
+import Borders from "./utility/borders";
+import Animation from "./utility/Animation";
+import Others from "./utility/other";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      
+       <Routes>
+        <Route path="/" element={<Dashboard/>}/>
+        <Route path="/components/buttons" element={<Button/>}/>
+        <Route path="/components/cards" element={<Cards/>}/>
+        <Route path="/pages/register" element={<Register/>}/>
+        <Route path="/pages/login" element={<LogInPage/>}/>
+        <Route path="/pages/forgot" element={<ForgotPassword/>}/>
+        <Route path="/pages/error" element={<ErrorPage/>}/>
+        <Route path="/pages/blank" element={<BlankPage/>}/>
+        <Route path="/others/tables" element={<TablesReact/>}/>
+        <Route path="/colors" element={<Colors/>}/>
+        <Route path="/borders" element={<Borders/>}/>
+        <Route path="/animation" element={<Animation/>}/>
+        <Route path="/others" element={<Others/>}/>
+          
+       </Routes>
+     </div>
   );
 }
 
---
+++ src/pages/BlankPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import BaseApp from "../Base/base";
+import "./page.css"
+
+function BlankPage(){
+    return(
+        <BaseApp>
+        <> 
+        <div className = "blankpage">BlankPage</div>
+        </>
+        </BaseApp>
+    )
+}
+
+export default BlankPage
